6b1d40fbf0ccc40010f5dd906746cf38f706e5f6
Uchenna Aduaka <uchennaaduka4@gmail.com>
2026-09-01 19:01:12 -0400

    fix infra paving

--- infrastructure/terraform/modules/compute/ecr/main.tf
+++ infrastructure/terraform/modules/compute/ecr/main.tf
@@ -2,6 +2,9 @@ locals {
   prefix = var.resource_prefix
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 resource "aws_ecr_repository" "companion" {
   name                 = var.companion_repository_name
   image_tag_mutability = "MUTABLE"
@@ -55,4 +58,31 @@ resource "aws_ecr_lifecycle_policy" "companion_kb" {
   repository = aws_ecr_repository.companion_kb.name
 
   policy = aws_ecr_lifecycle_policy.companion.policy
+}
+
+# Lambda pulls container images at create/update time (not via the function execution role).
+resource "aws_ecr_repository_policy" "companion_kb_lambda" {
+  repository = aws_ecr_repository.companion_kb.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrieval"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Condition = {
+          StringLike = {
+            "aws:sourceArn" = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:*"
+          }
+        }
+      }
+    ]
+  })
 }
